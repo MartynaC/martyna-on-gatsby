6ccb9e11397b06be4b5813344e1f0a8caf57a589
MartynaC <martynach@ymail.com>
2023-02-15 00:39:10 +0100

    adding tap bottom navbar to the mobile

---
+++ src/components/bottomNav.js
@@ -0,0 +1,56 @@
+import React, { useRef, useEffect } from 'react';
+import { Link } from 'gatsby';
+import { useState } from 'react';
+import "bootstrap/dist/js/bootstrap.min.js";
+
+const BottomNav = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+  const dropdownRef = useRef(null)
+
+  useEffect(() => {
+    const dropdown = dropdownRef.current;
+    const dropdownMenu = dropdown.querySelector(".dropdown-menu");
+
+    dropdown.addEventListener("click", (e) => {
+      e.preventDefault();
+      dropdownMenu.classList.toggle("show");
+      setIsOpen(!isOpen);
+    });
+
+    const dropdownLinks = dropdown.querySelectorAll(".dropdown-item");
+
+    dropdownLinks.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        setIsOpen(false);
+        const href = link.getAttribute('href');
+        if (href) {
+          window.location.href = href;
+        }
+      });
+    });
+  }, [isOpen]);
+
+  
+    return (
+      <nav className="bottom-nav d-md-none">
+        <ul>
+          <div className="dropdown dropup" ref={dropdownRef}>
+            <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Projects
+            </button>
+            <div className="dropdown-menu dropdown-menu-right dropdown-menu-up" aria-labelledby="dropdownMenuButton">
+              <Link to="/art" className="dropdown-item">CREATIVE</Link>
+              <Link to="/work" className="dropdown-item">COMMERCIAL</Link>
+            </div>
+          </div>        
+          <li><Link to="/about/">About</Link></li>
+          <li><Link to="/contact/">Contact</Link></li>
+        </ul>
+      </nav>
+    );
+  };
+  
+
+export default BottomNav;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,48 +1,41 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import { Link } from "gatsby"
-import Navbar from "./navbar"
-import "./layout.scss"
-import RotatingElement from 'react-move-element'
+import React, { useState } from 'react'
+import Navbar from './navbar'
+import BottomNav from './bottomNav'
+import './layout.scss'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <div className="container-fluid p-0">
-   
-    
-      <main>
+      <div className="d-none d-md-block">
         <div className="row">
-      <div className="col-md-3 col-xs-12 sticky">
-{/*
-      <Link to="/"><img src="/tetha.webp" className="logo" alt="logo" /></Link> */}
-
-     
-      <Navbar siteTitle={data.site.siteMetadata?.title || `Title`} />
+          <div className="col-md-3 sticky">
+            <Navbar />
+          </div>
+          <div className="col-md-9 scrollable-content">
+            {children}
+          </div>
+        </div>
       </div>
-      <div className="col-md-9 col-xs-12 scrollable-content">
-        {children}
+      <div className="d-md-none">
+        <div className="fixed-bottom">
+          <BottomNav isOpen={isOpen} toggleMenu={toggleMenu} />
         </div>
+        <div className={`overlay ${isOpen ? 'open' : ''}`} onClick={toggleMenu} />
+        <div className={`mobile-menu ${isOpen ? 'open' : ''}`}>
+    
         </div>
-      </main>
-     
+        <div className="scrollable-content">
+          {children}
+        </div>
+      </div>
     </div>
-
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
